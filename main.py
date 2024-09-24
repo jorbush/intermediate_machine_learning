a65a8c2830fec041e350e3689e522dc3216c984f
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-24 21:34:11 +0200

    missing values

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
@@ -62,6 +63,167 @@ def introduction_exercise():
         model_id = i + 1
         print("Model %d MAE: %d" % (model_id, mae))
 
-if __name__ == '__main__':
-    introduction_exercise()
+def missing_values():
+    # Load the data
+    data = pd.read_csv('./input/melb_data.csv')
+    # Select target
+    y = data.Price
+    # To keep things simple, we'll use only numerical predictors
+    melb_predictors = data.drop(['Price'], axis=1)
+    X = melb_predictors.select_dtypes(exclude=['object'])
+    # Divide data into training and validation subsets
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8,
+                                                          test_size=0.2, random_state=0)
 
+    # Function for comparing different approaches
+    def score_dataset(X_train, X_valid, y_train, y_valid):
+        model = RandomForestRegressor(n_estimators=10, random_state=0)
+        model.fit(X_train, y_train)
+        preds = model.predict(X_valid)
+        return mean_absolute_error(y_valid, preds)
+
+    # Get names of columns with missing values
+    cols_with_missing = [col for col in X_train.columns
+                         if X_train[col].isnull().any()]
+
+    # Drop columns in training and validation data
+    reduced_X_train = X_train.drop(cols_with_missing, axis=1)
+    reduced_X_valid = X_valid.drop(cols_with_missing, axis=1)
+
+    print("MAE from Approach 1 (Drop columns with missing values):")
+    print(score_dataset(reduced_X_train, reduced_X_valid, y_train, y_valid))
+
+    # Imputation
+    my_imputer = SimpleImputer()
+    imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+    imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+
+    # Imputation removed column names; put them back
+    imputed_X_train.columns = X_train.columns
+    imputed_X_valid.columns = X_valid.columns
+
+    print("MAE from Approach 2 (Imputation):")
+    print(score_dataset(imputed_X_train, imputed_X_valid, y_train, y_valid))
+
+    # Make copy to avoid changing original data (when imputing)
+    X_train_plus = X_train.copy()
+    X_valid_plus = X_valid.copy()
+
+    # Make new columns indicating what will be imputed
+    for col in cols_with_missing:
+        X_train_plus[col + '_was_missing'] = X_train_plus[col].isnull()
+        X_valid_plus[col + '_was_missing'] = X_valid_plus[col].isnull()
+
+    # Imputation
+    my_imputer = SimpleImputer()
+    imputed_X_train_plus = pd.DataFrame(my_imputer.fit_transform(X_train_plus))
+    imputed_X_valid_plus = pd.DataFrame(my_imputer.transform(X_valid_plus))
+
+    # Imputation removed column names; put them back
+    imputed_X_train_plus.columns = X_train_plus.columns
+    imputed_X_valid_plus.columns = X_valid_plus.columns
+
+    print("MAE from Approach 3 (An Extension to Imputation):")
+    print(score_dataset(imputed_X_train_plus, imputed_X_valid_plus, y_train, y_valid))
+
+    # Shape of training data (num_rows, num_columns)
+    print(X_train.shape)
+
+    # Number of missing values in each column of training data
+    missing_val_count_by_column = (X_train.isnull().sum())
+    print(missing_val_count_by_column[missing_val_count_by_column > 0])
+
+def missing_values_exercise():
+    # Read the data
+    X_full = pd.read_csv('./input/train.csv', index_col='Id')
+    X_test_full = pd.read_csv('./input/test.csv', index_col='Id')
+    # Remove rows with missing target, separate target from predictors
+    X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+    y = X_full.SalePrice
+    X_full.drop(['SalePrice'], axis=1, inplace=True)
+    # To keep things simple, we'll use only numerical predictors
+    X = X_full.select_dtypes(exclude=['object'])
+    X_test = X_test_full.select_dtypes(exclude=['object'])
+    # Break off validation set from training data
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8,
+                                                          test_size=0.2, random_state=0)
+    print(X_train.head())
+
+    # Shape of training data (num_rows, num_columns)
+    print(X_train.shape)
+
+    # Number of missing values in each column of training data
+    missing_val_count_by_column = (X_train.isnull().sum())
+    print(missing_val_count_by_column[missing_val_count_by_column > 0])
+
+    # Fill in the line below: How many rows are in the training data?
+    num_rows = X_train.shape[0]
+    print(num_rows)
+
+    # Fill in the line below: How many columns in the training data
+    # have missing values?
+    num_cols_with_missing = len([col for col in X_train.columns
+                         if X_train[col].isnull().any()])
+    print(num_cols_with_missing)
+
+    # Fill in the line below: How many missing entries are contained in
+    # all of the training data?
+    tot_missing = X_train.isnull().sum().sum()
+    print(tot_missing)
+
+    # Function for comparing different approaches
+    def score_dataset(X_train, X_valid, y_train, y_valid):
+        model = RandomForestRegressor(n_estimators=100, random_state=0)
+        model.fit(X_train, y_train)
+        preds = model.predict(X_valid)
+        return mean_absolute_error(y_valid, preds)
+
+    # Fill in the line below: get names of columns with missing values
+    cols_with_missing = [col for col in X_train.columns
+                         if X_train[col].isnull().any()]
+
+    # Fill in the lines below: drop columns in training and validation data
+    reduced_X_train = X_train.drop(cols_with_missing, axis=1)
+    reduced_X_valid = X_valid.drop(cols_with_missing, axis=1)
+
+    print("MAE (Drop columns with missing values):")
+    print(score_dataset(reduced_X_train, reduced_X_valid, y_train, y_valid))
+
+    # Fill in the lines below: imputation
+    my_imputer = SimpleImputer()
+    imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+    imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+
+    # Fill in the lines below: imputation removed column names; put them back
+    imputed_X_train.columns = X_train.columns
+    imputed_X_valid.columns = X_valid.columns
+
+    print("MAE (Imputation):")
+    print(score_dataset(imputed_X_train, imputed_X_valid, y_train, y_valid))
+
+    # Preprocessed training and validation features
+    my_imputer = SimpleImputer()
+    final_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+    final_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+
+    # Define and fit model
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(final_X_train, y_train)
+
+    # Get validation predictions and MAE
+    preds_valid = model.predict(final_X_valid)
+    print("MAE (Your approach):")
+    print(mean_absolute_error(y_valid, preds_valid))
+
+    # Fill in the line below: preprocess test data
+    final_X_test = pd.DataFrame(my_imputer.transform(X_test))
+
+    # Fill in the line below: get test predictions
+    preds_test = model.predict(final_X_test)
+
+
+
+if __name__ == '__main__':
+    # introduction_exercise()
+    # missing_values()
+    missing_values_exercise()
